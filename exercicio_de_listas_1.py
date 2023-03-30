f8c4be06927dd0f5e03bcda0b6cb696ab2e27092
unknown <leticiafcassimiro@gmail.com>
2023-03-30 16:48:14 -0300

    Realização do segundo exercício, com criação e exibição de uma lista com 10 números, na ordem inversa, utilizando da estrutura de repetição 'for'.

--- exercicio_de_listas_1.py
+++ exercicio_de_listas_1.py
@@ -1,3 +1,2 @@
-#exercício 1
 listaDeNumerosInteiros = [3, 7, 16, 41, 66]
 print(listaDeNumerosInteiros)
---
+++ exercicio_de_listas_2.py
@@ -0,0 +1,7 @@
+listaDeNumerosReais = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+novaLista = []
+
+for i in range (0, 10):
+    novaLista.append(listaDeNumerosReais.pop())
+
+print(novaLista)
