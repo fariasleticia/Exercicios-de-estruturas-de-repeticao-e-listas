bb924c82cdf4fe15d138ec6f449a755843cef6c1
unknown <leticiafcassimiro@gmail.com>
2023-03-30 17:10:01 -0300

    Realização do terceiro exercício, com realização de uma lista de notas e exibição delas e da média da lista.

---
+++ exercicio_de_listas_3.py
@@ -0,0 +1,13 @@
+notas = [7, 8, 9, 10]
+print('Notas da galerinha:', notas)
+
+quantidade = 0
+soma = 0
+
+for valores in notas:
+    quantidade = quantidade + 1
+    soma = soma + valores
+
+print('Média das notas da galerinha:', soma/quantidade)
+
+#não entendi como essa bagaça funciona :)
