e272fa4acce89ecbe591ed7b4a13aee1dce737c8
unknown <leticiafcassimiro@gmail.com>
2023-04-06 15:52:21 -0300

    Foi editado o exercício 4, com criação de uma lista vazia para consoantes e uso da funcionalidade append para adicionar as consoantes da palavra ao vetor criado. Foi realizado o exercício 5, com criação de um vetor com 20 números inteiros e a conseguinte alocação dos dados em duas listas, uma para pares e outra para ímpares, através do uso de um loop for e de uma estrutura condicional que utiliza a condição de divisibilidade por 2.

--- exercicio_de_listas_4.py
+++ exercicio_de_listas_4.py
@@ -1,11 +1,14 @@
 palavra_maneira = ['m', 'e', 'l', 'a', 'n', 'c', 'o', 'l', 'i', 'a']
 vogais = ['a', 'e', 'i', 'o', 'u']
+consoantes = []
 
 numero_consoantes = 0
 
 for letras in palavra_maneira:
     if letras not in vogais:
         numero_consoantes = numero_consoantes + 1
-        print(letras)
+        consoantes.append(letras)
 
+print('Palavra maneira:', palavra_maneira)
+print('Consoantes da palavra maneira:', consoantes)
 print('Total de consoantes:', numero_consoantes)
