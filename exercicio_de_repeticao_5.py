29578e4050976f9cfeadedd6f0a1e865b68a91d7
Leticia Farias Cassimiro <leticiafcassimiro@gmail.com>
2023-04-14 22:42:41 -0300

    Edição do exercício 5, adicionando ao final uma funcionalidade que permite que o usuário decida se quer repetir o processo ou não, usando while e if.

--- exercicio_de_repeticao_5.py
+++ exercicio_de_repeticao_5.py
@@ -1,46 +1,58 @@
-validade_paisA = False
-while validade_paisA == False:
-    print("Insira o número total da população do país Abacate:")
-    entrada_paisA = float(input())
-    if entrada_paisA > 0:
-        print ("Ok!")
-        validade_paisA = True        
-    else:
-        print ("Este não é um valor válido.")
+def calculos_irados_competicao_cresc_paises():
+    validade_paisA = False
+    while validade_paisA == False:
+        print("Insira o número total da população do país Abacate:")
+        entrada_paisA = float(input())
+        if entrada_paisA > 0:
+            print ("Ok!")
+            validade_paisA = True        
+        else:
+            print ("Este não é um valor válido.")
 
-validade_taxaA = False
-while validade_taxaA == False:
-    print("Insira a taxa de crescimento da população abacateira (em decimais):")
-    entrada_taxaA = float(input())
-    if entrada_taxaA > 0 and entrada_taxaA <= 1:
-        print ("Ok!")
-        validade_taxaA = True        
-    else:
-        print ("Este não é um valor válido.")
+    validade_taxaA = False
+    while validade_taxaA == False:
+        print("Insira a taxa de crescimento da população abacateira (em decimais):")
+        entrada_taxaA = float(input())
+        if entrada_taxaA > 0:
+            print ("Ok!")
+            validade_taxaA = True        
+        else:
+            print ("Este não é um valor válido.")
 
-validade_paisB = False
-while validade_paisB == False:
-    print("Insira o número total da população do país Bolinho:")
-    entrada_paisB = float(input())
-    if entrada_paisB > 0:
-        print ("Ok!")
-        validade_paisB = True        
-    else:
-        print ("Este não é um valor válido.")
+    validade_paisB = False
+    while validade_paisB == False:
+        print("Insira o número total da população do país Bolinho:")
+        entrada_paisB = float(input())
+        if entrada_paisB > 0:
+            print ("Ok!")
+            validade_paisB = True        
+        else:
+            print ("Este não é um valor válido.")
 
-validade_taxaB = False
-while validade_taxaB == False:
-    print("Insira a taxa de crescimento da população bolinhos (em decimais):")
-    entrada_taxaB = float(input())
-    if entrada_taxaB > 0 and entrada_taxaB <=1:
-        print ("Ok!")
-        validade_taxaB = True
-    else:
-        print ("Este não é um valor válido.")
+    validade_taxaB = False
+    while validade_taxaB == False:
+        print("Insira a taxa de crescimento da população bolinhos (em decimais):")
+        entrada_taxaB = float(input())
+        if entrada_taxaB > 0:
+            print ("Ok!")
+            validade_taxaB = True
+        else:
+            print ("Este não é um valor válido.")
 
-ano = 0
-while entrada_paisB > entrada_paisA:
-    ano = ano + 1
-    entrada_paisA = entrada_paisA * (entrada_paisA + 1)
-    entrada_paisB = entrada_paisB * (entrada_paisB + 1)
-print("Estes são os anos necessários para que a população abacate ultrapasse a bolinho:", ano)
+    ano = 0
+    while entrada_paisB > entrada_paisA:
+        ano = ano + 1
+        entrada_paisA = entrada_paisA * (entrada_paisA + 1)
+        entrada_paisB = entrada_paisB * (entrada_paisB + 1)
+    print("Estes são os anos necessários para que a população abacate ultrapasse a bolinho:", ano)
+
+parar_programa = False
+
+while parar_programa == False:
+    print("Olá viajante (que só dificulta minha vida)!")
+    calculos_irados_competicao_cresc_paises()
+    print("Legal! Quer fazer isso de novo? Insira 'sim' ou 'não'.\n(fala não)")
+    repetir_processo = input()
+    if repetir_processo == 'não' or repetir_processo == 'nao':
+        print("MUITO OBRIGADA! :D")
+        parar_programa = True
